7eeb55baf66aceb9a1d972d56451e03400e74934
domoncasle@gmail.com <domoncasle@gmail.com>
2017-11-09 14:13:53 -0600

    CustomMiddleWare example with notes

---
+++ CustomMiddleWare/CustomMiddleWare/MyMiddlewareClass.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare
+{
+    public class MyMiddlewareClass
+    {
+        RequestDelegate _next;
+        ILoggerFactory _loggerFactory; // dependency injection  from below constructor to send this logger in 
+        IOptions<MyMiddlewareOptions> _options;
+
+        public MyMiddlewareClass (RequestDelegate next, ILoggerFactory loggerFactory, IOptions<MyMiddlewareOptions> options)
+        {
+            _loggerFactory = loggerFactory;
+            _next = next;
+            _options = options;
+        }
+
+        public async Task Invoke (HttpContext context)
+        {
+            _loggerFactory.AddConsole();
+            var logger = _loggerFactory.CreateLogger("my own logger");
+            logger.LogInformation("my middle ware class is handling the request.");
+
+            context.Items["message"] = "The weather is nice"; // to pass data between the middleware us this 'IDictonary<object, object>'
+
+            await context.Response.WriteAsync(_options.Value.OptionOne + "\n"); // reference options defined in appsettings but placed into IOptions
+            await context.Response.WriteAsync("My middle ware class stuff\n");
+            await _next.Invoke(context);
+            await context.Response.WriteAsync("End of my middle ware class stuff\n");
+        }
+    }
+
+    public class MyMiddlewareOptions
+    {
+        public string OptionOne { get; set; }
+    }
+
+    public static class MyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddlewareClass>();
+        }
+    }
+}
---
+++ CustomMiddleWare/CustomMiddleWare/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace CustomMiddleWare
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; } // for configuration
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true); // point the configuration builder at a 'local' file
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions(); // Add options to services
+
+            var myOptions = Configuration.GetSection("MyMiddlewareOptionsSec"); // Add the settings section in the json file
+            services.Configure<MyMiddlewareOptions>(o => o.OptionOne = myOptions["OptionOne"]); // add to the services this configuration 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from component one!\n"); // Runs this then moves into next component.
+                await next.Invoke(); // Points to the below run statement to run 'next'
+                await context.Response.WriteAsync("Hello from component one AGAIN!\n"); // On return from below component we run this.
+            });
+
+            app.UseMyMiddleware(); // call my custom middle ware.
+
+            app.Map("/mymapbranch", (appbuilder) => // map this incoming path to THIS pipeline.
+            {
+                appbuilder.Use(async (context, next) => // Notice this is not "app" anymore but "appbuilder" to hold to this path
+                {
+                    await context.Response.WriteAsync("Custom middle ware\n");
+                    await next.Invoke();
+                });
+
+                appbuilder.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Greetings from my map branch!\n");
+                });
+            });
+
+            app.MapWhen(context => context.Request.Query.ContainsKey("querybranch"), (appbuilder) => // map this for incoming items that have a query string containing 'querybranch'
+            {
+                appbuilder.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("At query branch\n");
+                });
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(context.Items["message"].ToString() + "\n"); // referenced passed items from components by using the dictonary as such
+                await context.Response.WriteAsync("Hello World!\n");
+            });
+        }
+    }
+}
